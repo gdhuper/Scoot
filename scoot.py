b483196bbce7760e8c2447826feffc0d4a0f7e44
Gurpreet Singh <gdhuper@gmail.com>
2017-05-17 12:26:08 -0700

    reading data using pandas

---
+++ scoot.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
+
+
+def readAndFilterData():
+	#reading csv data file using pandas 
+	scoot_rides__test_data = pd.read_csv(sys.argv[1])
+
+	print(scoot_rides_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if len(sys.args) > 1:
+	readAndFilterData()
+else:
+	print("usage: scoot.py <path-to-csv-file>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
